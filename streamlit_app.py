8a3b1d411bc8d3d6e2f89ed5d37a5555118926f8
SanjanaDhanraj <105208916+SanjanaDhanraj@users.noreply.github.com>
2022-05-20 19:22:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruits_selected=streamlit.multiselect("pick some fruits:",list(my_fruit_list.ind
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)                      
-def get_fruityvice_data(this fruit_choice)
+def get_fruityvice_data(this_fruit_choice)
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 returnfruityvice_normalized
